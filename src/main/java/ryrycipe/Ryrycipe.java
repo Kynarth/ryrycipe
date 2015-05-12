8e5a376b8c57a17e4576656a1b35ec7ce94a4458
Nicolas Belouin <belouin.nicolas@gmail.com>
2015-05-12 14:54:38 +0200

    added RecipeCreator to the main layout.

--- src/main/java/ryrycipe/Ryrycipe.java
+++ src/main/java/ryrycipe/Ryrycipe.java
@@ -3,6 +3,7 @@ package ryrycipe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -14,6 +15,7 @@ import java.io.IOException;
 public class Ryrycipe extends Application {
 
     private Stage primaryStage;
+    private BorderPane rootLayout;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -21,24 +23,40 @@ public class Ryrycipe extends Application {
         this.primaryStage.setTitle("Ryrycipe");
 
         initialize();
+        showRecipeCreator();
 
         this.primaryStage.show();
     }
 
     /**
+     * Load the application's base UI.
      *
+     * @throws IOException
      */
     private void initialize() throws IOException {
         // Load the main layout from fxml file
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(this.getClass().getResource("view/RyrycipeView.fxml"));
-        BorderPane borderPane = loader.load();
+        rootLayout = loader.load();
 
         // Create a scene with the loaded layout
-        Scene scene = new Scene(borderPane, 600, 400);
-        primaryStage.setResizable(false);
+        Scene scene = new Scene(rootLayout, 600, 445);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    /**
+     * Load the interface for creating recipes.
+     *
+     * @throws IOException
+     */
+    private void showRecipeCreator() throws IOException {
+        // Load RecipeCreator fxml file
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("view/RecipeCreator.fxml"));
+        SplitPane recipeCreator = loader.load();
+
+        rootLayout.setCenter(recipeCreator);
     }
 
     /**
