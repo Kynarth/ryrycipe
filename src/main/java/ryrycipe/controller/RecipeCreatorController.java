eb2aa64432cea6608f080bdf860c66ba69ee2988
Nicolas Belouin <belouin.nicolas@gmail.com>
2015-05-18 10:31:47 +0200

    Populated planQualityCB, disabled controls composing the material filter.

--- src/main/java/ryrycipe/controller/RecipeCreatorController.java
+++ src/main/java/ryrycipe/controller/RecipeCreatorController.java
@@ -1,7 +1,12 @@
 package ryrycipe.controller;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import ryrycipe.util.PropertiesUtil;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,8 +16,24 @@ import java.util.ResourceBundle;
  */
 public class RecipeCreatorController implements Initializable {
 
+    @FXML
+    private ComboBox<String> planQualityCB;
+
+    @FXML
+    private ComboBox<String> planCB;
+
+    private ObservableList<String> planItems = FXCollections.observableArrayList();
+    private ObservableList<String> planQualityItems = FXCollections.observableArrayList();
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Initialize the combobox containing the different plan's quality
+        planQualityItems.addAll(resources.getString("combobox.quality.items").split(","));
+        planQualityCB.setItems(planQualityItems);
+        planQualityCB.setValue(planQualityItems.get(planQualityItems.size() -1));
+
+        // Initialize the combobox containing the different plans
 
     }
 }
---
+++ src/main/java/ryrycipe/dao/DAO.java
@@ -0,0 +1,7 @@
+package ryrycipe.dao;
+
+/**
+ * Created by kynarth on 18/05/15.
+ */
+public class DAO {
+}
