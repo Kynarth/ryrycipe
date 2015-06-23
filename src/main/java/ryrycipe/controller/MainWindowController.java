83922a784e3c899527b59e9889777fd1b2fbe195
Nicolas Belouin <belouin.nicolas@gmail.com>
2015-06-23 10:00:55 +0200

    Created fxml file corresponding the app's main window.

---
+++ src/main/java/ryrycipe/controller/MainWindowController.java
@@ -0,0 +1,18 @@
+package ryrycipe.controller;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Manage MainWindow's controls.
+ */
+public class MainWindowController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
